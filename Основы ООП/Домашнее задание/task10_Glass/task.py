502ce9c9099eb736374f7da0d215d87fffc7e57a
@arkivanov <100686042+arkadiyivanov@users.noreply.github.com>
2022-06-02 15:31:36 +0300

    PY200

--- Основы ООП/Домашнее задание/task10_Glass/task.py
+++ Основы ООП/Домашнее задание/task10_Glass/task.py
@@ -1,6 +1,13 @@
-# TODO написать класс Glass согласно условию
+class Glass:
+    def __init__(self, material: str):
+        self.material = material
+
+    def get_material(self):
+        return self.material # TODO написать класс Glass согласно условию
 
 
 if __name__ == "__main__":
-    ...
+    glass_1 = Glass(100)
+    print(glass_1.material)
+
 
--- Основы ООП/Домашнее задание/task9_Date/task.py
+++ Основы ООП/Домашнее задание/task9_Date/task.py
@@ -1,1 +1,33 @@
-# TODO class Date
+class Data:
+    def __init__(self, day: int, manth: int, year: int) -> int:
+        if not isinstance(day, int):
+            raise TypeError
+        if day < 10:
+            day = '0' + str(day)
+        self.day = day
+        if not isinstance(manth, int):
+            raise TypeError
+        if manth < 10:
+            manth = '0' + str(manth)
+        elif manth > 12:
+            raise ValueError
+        self.manth = manth
+        if not isinstance(year, int):
+            raise TypeError
+        self.year = year
+
+    def __repr__(self) -> str:
+        return f"Data({self.day}, {self.manth}, {self.year})"
+
+    def __str__(self) -> str:
+        return f"{self.day}/{self.manth}/{self.year}"
+
+
+if __name__ == '__main__':
+    data_1 = Data(5, 5, 1998)
+    data_2 = Data(11, 12, 1981)
+    data_3 = Data(2,1,1981)
+
+    print(data_1)
+    print(data_2)
+    print(data_3)
